4ecbe4cf8bdb20b85dcbf1787f635b25703c9429
经理 <manager@163.com>
2018-05-25 01:44:35 +0800

    third

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
 first = 'knoweledge is power'
 second = 'wherer there is a will, there is a way'
 third = 'never give up!'
-fourth = 'Eat to live, but do not live to eat'
+fourth = 'Eat to live, but do not live to eat!'
